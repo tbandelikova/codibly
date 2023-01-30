424c0fef1c19cf9cc445eff8348cdddd59f3bae2
tat.bandelikova <tat.bandelikova@gmail.com>
2023-01-30 18:09:51 +0300

    fix: add table with bg

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import { useState, useEffect } from 'react';
 import { Container, Box } from '@mui/system';
 import TextField from '@mui/material/TextField';
-import { List, ListItem, ListItemText, ListItemButton } from '@mui/material';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 import TablePagination from '@mui/material/TablePagination';
 
 const BASE_URL = 'https://reqres.in/api/products/';
@@ -33,20 +39,31 @@ export default function App() {
     <Container maxWidth="sm">
       <Box sx={{ py: 2, height: '100vh' }}>
         <TextField id="outlined-basic" label="Filter by id" variant="outlined" type="number" size="small" />
-        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-          {prod.length &&
-            prod.map((item) => {
-              return (
-                <ListItem key={item.id} >
-                  <ListItemButton >
-                    <ListItemText primary={item.id} />
-                    <ListItemText primary={item.name} />
-                    <ListItemText primary={item.year} />
-                  </ListItemButton>
-                </ListItem>
-              );
-            })}
-        </List>
+        <TableContainer component={Paper} sx={{ marginTop: 1 }}>
+          <Table aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Id</TableCell>
+                <TableCell align="left">Name</TableCell>
+                <TableCell align="right">Year</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {prod.map((item) => (
+                <TableRow
+                  key={item.id}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 }, backgroundColor: item.color }}
+                >
+                  <TableCell component="th" scope="row">
+                    {item.id}
+                  </TableCell>
+                  <TableCell align="left">{item.name.toUpperCase()}</TableCell>
+                  <TableCell align="right">{item.year}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
         <TablePagination
           component="div"
           count={12}
